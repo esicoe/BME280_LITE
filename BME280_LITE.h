e5f439287a0e1a35d863a4d518058ca42e072a9f
Edward Sicoe <166658855+esicoe@users.noreply.github.com>
2025-08-22 15:40:04 -0400

    Update BME280_LITE.h

--- BME280_LITE.h
+++ BME280_LITE.h
@@ -4,7 +4,14 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-// TELEMETRY REGISTERS -- READ
+// BME280 REGISTERS
+  #define CTRL_HUM    0xF2
+  #define CTRL_MEAS   0xF4
+  #define CONFIG      0xF5
+
+  #define DIG_T1_LSB  0x88
+  #define DIG_H2_LSB  0xE1
+
   #define PRESS_MSB     0xF7
   #define PRESS_LSB     0xF8
   #define PRESS_XLSB    0xF9
@@ -14,101 +21,70 @@
   #define HUM_MSB       0xFD 
   #define HUM_LSB       0xFE 
 
-// CONTROL REGISTERS -- READ && WRITE
-  #define CTRL_HUM      0xF2 
-  #define CTRL_MEAS     0xF4 
-  #define CONFIG        0xF5  
+// BME280 SETTINGS
+  #define BME_H_SKIP 0
+  #define BME_H_X1   1
+  #define BME_H_X2   2
+  #define BME_H_X4   3
+  #define BME_H_X8   4
+  #define BME_H_X16  5
 
-// DIGITAL CALIBRATION REGISTERS -- READ
-  #define DIG_T1_LSB    0x88  
-  #define DIG_T1_MSB    0x89 
-  #define DIG_T2_LSB    0x8A 
-  #define DIG_T2_MSB    0x8B  
-  #define DIG_T3_LSB    0x8C 
-  #define DIG_T3_MSB    0x8D 
+  #define BME_T_SKIP 0
+  #define BME_T_X1   1
+  #define BME_T_X2   2
+  #define BME_T_X4   3
+  #define BME_T_X8   4
+  #define BME_T_X16  5
 
-  #define DIG_P1_LSB    0x8E 
-  #define DIG_P1_MSB    0x8F 
-  #define DIG_P2_LSB    0x90 
-  #define DIG_P2_MSB    0x91 
-  #define DIG_P3_LSB    0x92 
-  #define DIG_P3_MSB    0x93
-  #define DIG_P4_LSB    0x94
-  #define DIG_P4_MSB    0x95
-  #define DIG_P5_LSB    0x96
-  #define DIG_P5_MSB    0x97 
-  #define DIG_P6_LSB    0x98
-  #define DIG_P6_MSB    0x99
-  #define DIG_P7_LSB    0x9A
-  #define DIG_P7_MSB    0x9B
-  #define DIG_P8_LSB    0x9C
-  #define DIG_P8_MSB    0x9D
-  #define DIG_P9_LSB    0x9E
-  #define DIG_P9_MSB    0x9F
+  #define BME_P_SKIP 0
+  #define BME_P_X1   1
+  #define BME_P_X2   2
+  #define BME_P_X4   3
+  #define BME_P_X8   4
+  #define BME_P_X16  5
 
-  #define DIG_H1        0xA1
-  #define DIG_H2_LSB    0xE1
-  #define DIG_H2_MSB    0xE2
-  #define DIG_H3        0xE3
-  #define DIG_H4_H5_1   0xE4  
-  #define DIG_H4_H5_2   0xE5  
-  #define DIG_H5_MSB    0xE6
-  #define DIG_H6        0xE7
+  #define BME_SLEEP   0
+  #define BME_FORCED1 1
+  #define BME_FORCED2 2
+  #define BME_NORMAL  3
 
-enum bme280_settings : uint8_t {
-  BME_H_SKIP = 0,
-  BME_H_X1   = 1,
-  BME_H_X2   = 2,
-  BME_H_X4   = 3,
-  BME_H_X8   = 4,
-  BME_H_X16  = 5,  
+  #define BME_TSB_0_5MS   0
+  #define BME_TSB_62_5MS  1
+  #define BME_TSB_125MS   2
+  #define BME_TSB_250MS   3
+  #define BME_TSB_500MS   4
+  #define BME_TSB_1000MS  5
+  #define BME_TSB_10MS    6
+  #define BME_TSB_20MS    7
 
-  // CTRL_MEAS
-  BME_T_SKIP      = 0, 
-  BME_T_X1        = 1, 
-  BME_T_X2        = 2, 
-  BME_T_X4        = 3, 
-  BME_T_X8        = 4, 
-  BME_T_X16       = 5, 
-  BME_P_SKIP      = 0, 
-  BME_P_X1        = 1, 
-  BME_P_X2        = 2, 
-  BME_P_X4        = 3, 
-  BME_P_X8        = 4, 
-  BME_P_X16       = 5, 
+  #define BME_FILTER_OFF 0
+  #define BME_FILTER_2   1
+  #define BME_FILTER_4   2
+  #define BME_FILTER_8   3
+  #define BME_FILTER_16  4
 
-  // CONFIG
-  BME_SLEEP       = 0, 
-  BME_FORCED1     = 1, 
-  BME_FORCED2     = 2, 
-  BME_NORMAL      = 3, 
-  BME_TSB_0_5MS   = 0, 
-  BME_TSB_62_5MS  = 1, 
-  BME_TSB_125MS   = 2, 
-  BME_TSB_250MS   = 3, 
-  BME_TSB_500MS   = 4, 
-  BME_TSB_1000MS  = 5, 
-  BME_TSB_10MS    = 6, 
-  BME_TSB_20MS    = 7, 
-  BME_FILTER_OFF  = 0, 
-  BME_FILTER_2    = 1, 
-  BME_FILTER_4    = 2, 
-  BME_FILTER_8    = 3, 
-  BME_FILTER_16   = 4, 
+struct BME_RegisterData {
+  bool isValid;
+  uint8_t data[32];
+};
+
+struct BME_SensorData {
+  bool isValid;
+  float data;
 };
 
 class BME280_LITE {
   public:
-    bool initialize(uint8_t dev_addr, uint8_t osrs_H, uint8_t osrs_T, uint8_t osrs_P, uint8_t bme280_mode, uint8_t bme280_stby, uint8_t bme280_iir);
-    void calibrate(uint8_t dev_addr);
-    float readTemperature(uint8_t dev_addr);
-    float readPressure(uint8_t dev_addr);
-    float readHumidity(uint8_t dev_addr);
-    float readAltitude(uint8_t dev_addr, float current_sea_level_pressure);
+    bool begin(uint8_t BMEaddress, uint8_t BMEosrs_H, uint8_t BMEosrs_T, uint8_t BMEosrs_P, uint8_t BMEmode, uint8_t BMEstby, uint8_t BMEIIR);
+    bool calibrate(uint8_t BMEaddress);
+    BME_SensorData readTemperature(uint8_t BMEaddress);
+    BME_SensorData readPressure(uint8_t BMEaddress);
+    BME_SensorData readHumidity(uint8_t BMEaddress);
+    BME_SensorData readAltitude(uint8_t BMEaddress, float seaLevelPressure);
 
   private:
-    bool write(uint8_t dev_addr, uint8_t reg_addr, uint8_t reg_val);
-    void read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data_buf, uint8_t read_len);
+    BME_RegisterData read(uint8_t BMEaddress, uint8_t BMEregAddress, uint8_t BMEnumBytes);
+    bool write(uint8_t BMEaddress, uint8_t BMEregAddress, uint8_t BMEwriteData);
 
     uint16_t T1;
     int16_t T2, T3;
@@ -121,5 +97,4 @@ class BME280_LITE {
     int8_t H6;
     int32_t t_fine;
 };
-
 #endif
