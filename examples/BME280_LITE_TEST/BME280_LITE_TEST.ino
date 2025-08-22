5559f5ee14d0251043746cc7a931bebcf35150ff
Edward Sicoe <166658855+esicoe@users.noreply.github.com>
2025-08-22 15:41:37 -0400

    Update BME280_LITE_TEST.ino

--- examples/BME280_LITE_TEST/BME280_LITE_TEST.ino
+++ examples/BME280_LITE_TEST/BME280_LITE_TEST.ino
@@ -2,47 +2,68 @@
   This is the test for the BME280_LITE library.
   This library only works with I2C. The device's address is 0x76 or 0x77.
   The objective of the library is to be as simple and as lightweight as possible.
+  Version = 2.0.0 (changes include more error handling by setting function output as structs)
   
   Written by Edward Sicoe.
 */
 
-#include <Wire.h>
 #include <BME280_LITE.h>
 
-#define BME_ADDR 0x76 // Default BME280 I2C address.
-#define BME_XADDR 0x77 // Auxilliary BME280 I2C address, useful if you have two of these.
-#define PRESSURE_AT_SEA_LEVEL_hPa 1013.25 // Add the sea level pressure in your location. Improves accuracy big time.
+#define BME_ADDR    0x76 // Primary address (0x77 is the alternate)
+#define SEA_LEVEL_PRES  1017.8 // sea level pressure in hPa to use in the readAltitude funciton.
 
-BME280_LITE bme; // Constructor for the BME280-LITE class.
+BME280_LITE bme;
 
 void setup() {
+  bme.begin(BME_ADDR, // returns a T/F based on initialization.
+            BME_H_X1, // All settings can be found in the docs.
+            BME_T_X1, 
+            BME_P_X16, 
+            BME_NORMAL, 
+            BME_TSB_0_5MS, 
+            BME_FILTER_2);
+  bme.calibrate(BME_ADDR); // returns T/F based on calibration status. You can read it if you wish.
   Serial.begin(9600);
-  bool status = bme.initialize(BME_ADDR, // Specified I2C address from the preprocessor directive
-                BME_H_X1, // Oversampling for humidity. Check docs for more info.
-                BME_T_X1, // Oversampling for temperature. Check docs for more info.
-                BME_P_X2, // Oversampling for pressure. Check docs for more info.
-                BME_NORMAL, // Operating mode for the device. Check docs for more info.
-                BME_TSB_0_5MS, // Standby time, available only in normal mode. Check docs for more info.
-                BME_FILTER_2); // IIR filter coefficient. Check docs for more info.
-
-  if (!status) {
-  Serial.println("FAILED BME280 INITALIZATION"); // bme.initalize retuns a bool type given it's success.
-  // FALSE = failed to initalize, TRUE = I2C OK.
-  }
-
-  bme.calibrate(BME_ADDR); // Assigns important calibration values from the BME280. More info in docs. Returns nothing.
 }
 
 void loop() {
-  float temperature = bme.readTemperature(BME_ADDR); // Temperature is in celsius (float).
-  float humidity = bme.readHumidity(BME_ADDR); // Humidity in %RH (float).
-  float pressure = bme.readPressure(BME_ADDR); // Pressure is in hPa (float).
-  float altitude = bme.readAltitude(BME_ADDR, PRESSURE_AT_SEA_LEVEL_hPa); // Elevation is in meters (float).
+/* 
+Note: all read functions return a struct with a "isValid" boolean followed by a float "data".
+The struct BME_SensorData is used since a failed I2C transaction means that "isValid" is false.
+Reading when isValid is false means the read function is simply reading noise from the pin and is outputing false data.
+*/
 
-  Serial.print("T: "); Serial.println(temperature);
-  Serial.print("H: "); Serial.println(humidity);
-  Serial.print("P: "); Serial.println(pressure);
-  Serial.print("ALT: "); Serial.println(altitude);
+  BME_SensorData temperature = bme.readTemperature(BME_ADDR); 
+  if (!temperature.isValid) {
+    Serial.println("TEMP READ FAIL");
+  } else {
+    Serial.print("T: ");
+    Serial.println(temperature.data);
+  }
+  
+  BME_SensorData pressure = bme.readPressure(BME_ADDR);
+  if (!pressure.isValid) {
+    Serial.println("PRESSURE READ FAIL");
+  } else {
+    Serial.print("P: ");
+    Serial.println(pressure.data);
+  }
 
-  delay(1000); // 1s delay.
+  BME_SensorData humidity = bme.readHumidity(BME_ADDR);
+  if (!humidity.isValid) {
+    Serial.println("HUMIDITY READ FAIL");
+  } else {
+    Serial.print("H: ");
+    Serial.println(humidity.data);
+  }
+  
+  BME_SensorData altitude = bme.readAltitude(BME_ADDR, SEA_LEVEL_PRES);
+  if (!altitude.isValid) {
+    Serial.println("ALTITUDE READ FAIL");
+  } else {
+    Serial.print("ALT: ");
+    Serial.println(altitude.data);
+  }
+
+  delay(1000);
 }
